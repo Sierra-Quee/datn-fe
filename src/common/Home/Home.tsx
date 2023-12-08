dc49ee11ec0e4810f409d1366bc5893016a1ca1b
Sierra-Quee <phuong0611200@gmail.com>
2023-12-08 17:10:29 +0700

    [DATN-16] Chart in home

--- src/common/Home/Home.tsx
+++ src/common/Home/Home.tsx
@@ -6,20 +6,30 @@ import {
     UsergroupAddOutlined,
     UsergroupDeleteOutlined,
 } from "@ant-design/icons";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
+import { DatePicker, DatePickerProps, Select, Typography } from "antd";
+import { RangePickerProps } from "antd/es/date-picker";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 import {
     clearListService,
     getAllServiceAsync,
 } from "../../core/reducers/service";
 import { clearListSkill, getAllSkillAsync } from "../../core/reducers/skill";
 import { useAppDispatch, useAppSelector } from "../../redux/hook";
+import LineChartHome from "./LineChartHome";
 
 const Home = () => {
+    dayjs.extend(customParseFormat);
     const dispatch = useAppDispatch();
+    const { Title } = Typography;
+    const { RangePicker } = DatePicker;
 
     const { listSkill } = useAppSelector((state) => state.skill);
     const { listService } = useAppSelector((state) => state.service);
+    const [titleChart, setTitleChart] = useState<string>("");
+    const [timeRange, setTimeRange] = useState<[string, string] | string>("");
 
     useEffect(() => {
         handleGetAllSkillAsync();
@@ -30,7 +40,20 @@ const Home = () => {
             dispatch(clearListSkill());
         };
     }, [dispatch]);
-
+    const dateFormat = "YYYY/MM/DD";
+    const optionsChart: any = [
+        { value: "Skill", label: "Skill" },
+        { value: "Service", label: "Service" },
+        { value: "Customer", label: "Customer" },
+        {
+            value: "Repair",
+            label: "Repair",
+        },
+        {
+            value: "Revenue",
+            label: "Revenue",
+        },
+    ];
     const handleGetAllSkillAsync = async () => {
         await dispatch(getAllSkillAsync());
     };
@@ -38,14 +61,26 @@ const Home = () => {
     const handleGetAllServiceAsync = async () => {
         await dispatch(getAllServiceAsync());
     };
+    const handleChange = (value: string) => {
+        setTitleChart(value);
+    };
+    const onChangeRangeDate = (
+        value: DatePickerProps["value"] | RangePickerProps["value"],
+        dateString: [string, string] | string
+    ) => {
+        console.log("Selected Time: ", value);
+        console.log("Formatted Selected Time: ", dateString);
+        setTimeRange(dateString);
+    };
 
     return (
         <div className="home">
+            <Title level={3}>ISmart</Title>
             <div className="home-statistic">
                 <div className="home-statistic-item home-statistic-item-skill">
                     <span className="home-statistic-item-title">
                         <span>
-                            <UngroupOutlined /> Số loại dịch vụ:
+                            <UngroupOutlined /> Số loại dịch vụ
                         </span>
                         <div>{listSkill.length}</div>
                     </span>
@@ -53,7 +88,7 @@ const Home = () => {
                 <div className="home-statistic-item home-statistic-item-service">
                     <span className="home-statistic-item-title">
                         <span>
-                            <GlobalOutlined /> Số dịch vụ:
+                            <GlobalOutlined /> Số dịch vụ
                         </span>
                         <div>{listService.length}</div>
                     </span>
@@ -61,7 +96,7 @@ const Home = () => {
                 <div className="home-statistic-item home-statistic-item-customer">
                     <span className="home-statistic-item-title">
                         <span>
-                            <UsergroupAddOutlined /> Số khách hàng:
+                            <UsergroupAddOutlined /> Số khách hàng
                         </span>
                         <div>1128</div>
                     </span>
@@ -69,10 +104,35 @@ const Home = () => {
                 <div className="home-statistic-item home-statistic-item-employee">
                     <span className="home-statistic-item-title">
                         <span>
-                            <UsergroupDeleteOutlined /> Số thợ:
+                            <UsergroupDeleteOutlined /> Số thợ
                         </span>
                         <div>1128</div>
                     </span>
+                </div>
+            </div>
+            <div className="home-chart">
+                <Title level={3}>Thống kê hàng tháng</Title>
+                <div
+                    style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                    <Select
+                        defaultValue="Chọn biểu đồ"
+                        style={{ width: 200 }}
+                        onChange={handleChange}
+                        options={optionsChart}
+                    />
+                    <RangePicker
+                        defaultValue={[
+                            dayjs("2015/01/01", dateFormat),
+                            dayjs("2015/01/01", dateFormat),
+                        ]}
+                        format={dateFormat}
+                        style={{ width: 300 }}
+                        onChange={onChangeRangeDate}
+                    />
+                </div>
+                <div className="home-line-chart">
+                    <LineChartHome title={titleChart} date={timeRange} />
                 </div>
             </div>
         </div>
